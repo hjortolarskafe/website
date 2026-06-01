b1a2fa258faf06d032d888a31f2974a72fedb6e5
André Johansson <andre.johansson@storykit.io>
2026-06-01 15:49:46 +0200

    Fixes to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import node from 'astro/logger/node';
 
 // https://astro.build/config
 export default defineConfig({
